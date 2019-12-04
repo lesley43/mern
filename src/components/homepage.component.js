9e84f2d48f728ed04f8e1a9664e10f3c2658a2a7
lesley <lesleyiannarelli@gmail.com>
2019-12-04 15:41:12 -0500

    homepage

---
+++ src/components/homepage.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CropList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Home page</p>
+      </div>
+    )
+  }
+}
