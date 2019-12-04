b2a0245398ce9a77e5e47f484cbda9cd7a0446c9
lesley <lesleyiannarelli@gmail.com>
2019-12-04 15:38:40 -0500

    added homepage and calendars

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./components/navbar.component";
+import HomePage from "./components/homepage.component"
 import CropList from "./components/crops-list.component";
 import EditEvent from "./components/edit-event.component";
 import CreateCrop from "./components/create-crop.component";
@@ -13,7 +14,8 @@ function App() {
     <Router>
         <Navbar />
         <br/>
-        <Route path="/" exact component={CropList} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/crops" component={CropList} />
         <Route path="/create" component={CreateCrop} />
         <Route path="/event" component={CreateEvent} />
         <Route path="/edit/:id" component={EditEvent} />
