717069a059e6a676522baaca1873e791c0c6d49d
Justin Johns <43861450+justinrjohns@users.noreply.github.com>
2019-12-06 12:10:52 -0500

    Update navbar.component.js

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
-export default class Navbar extends React.Component {
-  render() {
+const Navbar = ({ user }) => {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">The Pocket Plot</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
-              <Link to="/crops" className="nav-link">Crops</Link>
+              <NavLink to="/crops" className="nav-link">Crops</NavLink>
             </li>
             <li className="navbar-item">
-              <Link to="/create" className="nav-link">Create Crop</Link>
+              <NavLink to="/create" className="nav-link">Create Crop</NavLink>
             </li>
             <li>
-              <Link to="/event" className="nav-link">Calendar</Link>
+              <NavLink to="/event" className="nav-link">Calendar</NavLink>
             </li>
+            <li>
+              {!user && (
+                <React.Fragment>
+                  <NavLink className="nav-item nav-link" to="/login">
+                    Login
+                  </NavLink>
+                  <NavLink className="nav-item nav-link" to="/register">
+                    Register
+                  </NavLink>
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {user && (
+                <React.Fragment>
+                  <NavLink className="nav-item nav-link" to="/profile">
+                    {user.name}
+                  </NavLink>
+                  <NavLink className="nav-item nav-link" to="/logout">
+                    Logout
+                  </NavLink>
+                </React.Fragment>
+              )}   
+            </li> 
           </ul>
         </div>
       </nav>
     )
-  }
 }
+
+export default Navbar;
