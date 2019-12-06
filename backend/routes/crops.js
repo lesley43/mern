d290fd7f0f464807c47fe89601070ed114f078a9
Justin Johns <43861450+justinrjohns@users.noreply.github.com>
2019-12-06 18:34:44 -0500

    Update crops.js

--- backend/routes/crops.js
+++ backend/routes/crops.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let Crop = require('../models/crops.model');
+let Crop = require('../models/crop');
 
 //first route endpoint that handles http get requests
 //this is localhost:5000/crops/
@@ -10,7 +10,7 @@ router.route('/').get((req, res) => {
 });
 
 //this is a post request
-router.route('/add').post((req, res) => {
+router.route('/').post((req, res) => {
   const name = req.body.name;
   const cropType = req.body.cropType;
   const plantSeason = req.body.plantSeason;
@@ -30,4 +30,40 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+//that's object id from the database (Atlas makes the id)
+router.route('/:id').get((req, res) => {
+  Crop.findById(req.params.id) //getting the id directly from the url
+    .then(crop => res.json(crop))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Crop.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Crop deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//must have all fields to update, can't have one or the other
+router.route('/:id').post((req, red) => {
+  Crop.findById(req.params.id) //find current crop
+    .then(crop => { 
+      crop.name = req.body.name,
+      crop.type = {
+        _id: type._id,
+        name: type.name
+      },
+      crop.plantSeason = req.body.plantSeason,
+      harvestSeason = req.body.harvestSeason,
+      lifeCycle = req.body.lifeCycle
+    
+      crop.save()
+        .then(() => res.json('Crop updated'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
+
+
